1d6fd45b1edc003acc8eb5334d1b8435269f6096
“Gerard <gerard@meijer.gs>
2014-12-13 21:40:22 +0100

    iptables discoverer

--- docker/captain_iptables_discoverer/latest/files/scripts/run.sh
+++ docker/captain_iptables_discoverer/latest/files/scripts/run.sh
@@ -59,8 +59,35 @@ write_iptables_rules_file () {
   mkdir -p $FILE_PATH
   cat /dev/null > "$(get_file_path_including_file_name)"
 
-  envs="$(env)"
+  # get all public ips from all zones
+  local etcd_tree=$(get_tree "$ETCD_BASE_PATH")
+  local public_ips="$(echo $etcd_tree | $dir/jq '.node.nodes[] as $av_zones | $av_zones.nodes[] | select(.key | contains("/hosts")) | .nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/public_ip")) | .value')"
+  # get private ips from this zone
+  if [[ ! -z "$ETCD_CURRENT_AVZONE_PATH" ]]; then
+    local etcd_tree=$(get_tree "$ETCD_CURRENT_AVZONE_PATH")
+    local private_ips="$(echo $etcd_tree | $dir/jq '.node.nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/private_ip")) | .value')"
+  fi
+  
+  # public ip rules
+  local public_ip_rules=""
+  while read -r public_ip; do
+    # remove all double quotes
+    public_ip=${public_ip//\"/}
+    local public_ip_rule="-A INPUT -p tcp -s $public_ip -j ACCEPT"$'\n'
+    local public_ip_rules="$public_ip_rules$public_ip_rule"
+  done <<< "$public_ips"
 
+  # private ip rules
+  local private_ip_rules=""
+  while read -r private_ip; do
+    # remove all double quotes
+    private_ip=${private_ip//\"/}
+    local private_ip_rule="-A INPUT -p tcp -s $private_ip -j ACCEPT"$'\n'
+    local private_ip_rules="$private_ip_rules$private_ip_rule"
+  done <<< "$private_ips"
+  
+  # extra rules
+  envs="$(env)"
   local extra_rules=""
   while read -r env; do
     if [[ $env == *"IPTABLES_RULE_"* ]]; then
@@ -71,10 +98,35 @@ write_iptables_rules_file () {
   done <<< "$envs"
 
   echo "$iptables_default_rules_start" >> "$(get_file_path_including_file_name)"
+  echo "# public ip lines" >> "$(get_file_path_including_file_name)"
+  echo "$public_ip_rules" >> "$(get_file_path_including_file_name)"
+  echo "# private ip lines" >> "$(get_file_path_including_file_name)"
+  echo "$private_ip_rules" >> "$(get_file_path_including_file_name)"
   echo "# extra lines" >> "$(get_file_path_including_file_name)"
   echo "$extra_rules" >> "$(get_file_path_including_file_name)"
   echo "$iptables_default_rules_end" >> "$(get_file_path_including_file_name)"
 }
 
-echo "writing iptables rules file at $(get_file_path_including_file_name)..."
-write_iptables_rules_file
+watch_iptables_rules_file () {
+  local end_loop=false
+  local current_file="$(get_file_path_including_file_name)"
+  current_rules="$(/usr/bin/stat -c%s $current_file)"
+  while [[ "$end_loop" != true ]]; do
+    FILE_NAME="iptable_rules_watch"
+    write_iptables_rules_file
+    local new_file="$(get_file_path_including_file_name)"
+    new_rules="$(/usr/bin/stat -c%s $new_file)"
+    if [[ "$current_rules" != "$new_rules" && "$new_rules" != "0" ]]; then
+      end_loop=true
+    fi
+    sleep "$REFRESH_TIME"
+  done
+}
+
+if [[ "$MODE" == "init" ]]; then
+  echo "writing iptables rules file at $(get_file_path_including_file_name)..."
+  write_iptables_rules_file
+else
+  echo "watching changes for iptables rules file at $(get_file_path_including_file_name)..."
+  watch_iptables_rules_file
+fi
