bd002518d40ea5f600224efeb085643424f049d6
“Gerard <gerard@meijer.gs>
2014-12-14 11:53:03 +0100

    start with extracting functions from individual run.shs

--- docker/captain_iptables_discoverer/latest/files/scripts/run.sh
+++ docker/captain_iptables_discoverer/latest/files/scripts/run.sh
@@ -4,7 +4,7 @@ set -e
 # include dependencies
 dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
-. "$dir/etcd_helper"
+. "$dir/captain_functions"
 
 read -r -d '' iptables_default_rules_start << EOM || true
 *filter
@@ -42,14 +42,10 @@ read -r -d '' iptables_default_rules_end << EOM || true
 COMMIT
 EOM
 
-get_file_path_including_file_name () {
-  echo "$FILE_PATH$FILE_NAME"
-}
-
 write_iptables_rules_file () {
   set -e
   mkdir -p $FILE_PATH
-  cat /dev/null > "$(get_file_path_including_file_name)"
+  cat /dev/null > "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
 
   # get all public ips from all zones
   local etcd_tree="$(get_tree $ETCD_BASE_PATH)"
@@ -93,30 +89,30 @@ write_iptables_rules_file () {
     fi
   done <<< "$envs"
 
-  echo "$iptables_default_rules_start"'\n' >> "$(get_file_path_including_file_name)"
+  echo "$iptables_default_rules_start"'\n' >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
   if [[ ! -z "$public_ip_rules" ]]; then
-    echo "# public ip lines" >> "$(get_file_path_including_file_name)"
-    echo "$public_ip_rules" >> "$(get_file_path_including_file_name)"
+    echo "# public ip lines" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
+    echo "$public_ip_rules" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
   fi
   if [[ ! -z "$private_ip_rules" ]]; then
-    echo "# private ip lines" >> "$(get_file_path_including_file_name)"
-    echo "$private_ip_rules" >> "$(get_file_path_including_file_name)"
+    echo "# private ip lines" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
+    echo "$private_ip_rules" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
   fi
   if [[ ! -z "$extra_rules" ]]; then
-    echo "# extra lines" >> "$(get_file_path_including_file_name)"
-    echo "$extra_rules" >> "$(get_file_path_including_file_name)"
+    echo "# extra lines" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
+    echo "$extra_rules" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
   fi
-  echo "$iptables_default_rules_end" >> "$(get_file_path_including_file_name)"
+  echo "$iptables_default_rules_end" >> "$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
 }
 
 watch_iptables_rules_file () {
   local end_loop=false
-  local current_file="$(get_file_path_including_file_name)"
+  local current_file="$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
   current_rules="$(/usr/bin/stat -c%s $current_file)"
   while [[ "$end_loop" != true ]]; do
     FILE_NAME="iptable_rules_watch"
     write_iptables_rules_file
-    local new_file="$(get_file_path_including_file_name)"
+    local new_file="$(get_file_path_including_file_name $FILE_PATH $FILE_NAME)"
     new_rules="$(/usr/bin/stat -c%s $new_file)"
     new_rules_content="$(cat $new_file)"
     if [[ "$current_rules" != "$new_rules" && ("$new_rules_content" == *"private ip lines"* || "$new_rules_content" == *"public ip lines"*) ]]; then
@@ -127,9 +123,9 @@ watch_iptables_rules_file () {
 }
 
 if [[ "$MODE" == "init" ]]; then
-  echo "writing iptables rules file at $(get_file_path_including_file_name)..."
+  echo "writing iptables rules file at $(get_file_path_including_file_name $FILE_PATH $FILE_NAME)..."
   write_iptables_rules_file
 else
-  echo "watching changes for iptables rules file at $(get_file_path_including_file_name)..."
+  echo "watching changes for iptables rules file at $(get_file_path_including_file_name $FILE_PATH $FILE_NAME)..."
   watch_iptables_rules_file
 fi
