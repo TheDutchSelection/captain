358fd602bfa36417a564c771df7a0cb49ba4ca1c
“Gerard <gerard@meijer.gs>
2014-12-20 22:15:13 +0100

    no returning of invalid keys

--- docker/captain_restart_orchestrator/latest/files/scripts/run.sh
+++ docker/captain_restart_orchestrator/latest/files/scripts/run.sh
@@ -27,35 +27,33 @@ while true; do
 
   # walk through all containers that need a restart
   while read -r need_restart_key; do
-    if [[ ! -z "$need_restart_key" ]]; then
-      echo "$need_restart_key is $need_restart_value, container needs restart"
-      # get the relevant apps path
-      relevant_etcd_app_path="$(get_app_path_from_app_key $need_restart_key)"
+    echo "$need_restart_key is $need_restart_value, container needs restart"
+    # get the relevant apps path
+    relevant_etcd_app_path="$(get_app_path_from_app_key $need_restart_key)"
 
-      echo "checking how many containers are restarting for $relevant_etcd_app_path..."
-      # count the current amount of restarts processing for the app path
-      current_restarts="$(count_keys_with_value_for_app $relevant_etcd_app_path $restart_key $restart_value)"
-      echo "$current_restarts"
+    echo "checking how many containers are restarting for $relevant_etcd_app_path..."
+    # count the current amount of restarts processing for the app path
+    current_restarts="$(count_keys_with_value_for_app $relevant_etcd_app_path $restart_key $restart_value)"
+    echo "$current_restarts"
 
-      # set the max restarts for this app
-      max_restarts=1
-      for app_exception in $APP_EXCEPTIONS
-      do
-        short_etcd_key="$(echo $app_exception | awk -F'##' '{print $1}')"
-        max_restart_value="$(echo $app_exception | awk -F'##' '{print $2}')"
-        if [[ "/$short_etcd_key" == *"$relevant_etcd_app_path"* ]]; then
-          max_restarts="$max_restart_value"
-          break
-        fi
-      done
-
-      echo "max restarts for $relevant_etcd_app_path is $max_restarts"
-      if [[ "$max_restarts" > "$current_restarts" ]]; then
-        echo "setting restart value for container with $need_restart_key"
-        set_restart_from_needs_restart_etcd_key "$need_restart_key"
-      else
-        echo "max restarts reached, not restarting"
+    # set the max restarts for this app
+    max_restarts=1
+    for app_exception in $APP_EXCEPTIONS
+    do
+      short_etcd_key="$(echo $app_exception | awk -F'##' '{print $1}')"
+      max_restart_value="$(echo $app_exception | awk -F'##' '{print $2}')"
+      if [[ "/$short_etcd_key" == *"$relevant_etcd_app_path"* ]]; then
+        max_restarts="$max_restart_value"
+        break
       fi
+    done
+
+    echo "max restarts for $relevant_etcd_app_path is $max_restarts"
+    if [[ "$max_restarts" > "$current_restarts" ]]; then
+      echo "setting restart value for container with $need_restart_key"
+      set_restart_from_needs_restart_etcd_key "$need_restart_key"
+    else
+      echo "max restarts reached, not restarting"
     fi
   done <<< "$need_restart_keys"
   sleep "$REFRESH_TIME"
