6e7f52fad8bb306e7c49c923139326e082ed97b4
Gerard Meijer <gerard@meijer.gs>
2015-01-14 20:31:17 +0100

    refactor publisher

--- docker/captain_publisher/latest/files/scripts/run.sh
+++ docker/captain_publisher/latest/files/scripts/run.sh
@@ -9,19 +9,19 @@ if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/captain_functions"
 
 while true; do
-  envs="$(env)"
+  envs=$(env)
 
   while read -r env; do
     # set every ETCD_VALUE_[NAME]
-    if [[ $env == "ETCD_VALUE_"* ]]; then
-      env_key="$(echo $env | awk -F'=' '{print $1}')"
-      value="$(echo $env | awk -F'=' '{print $2}')"
+    if [[ "$env" == "ETCD_VALUE_"* ]]; then
+      env_key=$(echo "$env" | awk -F'=' '{print $1}')
+      value=$(echo "$env" | awk -F'=' '{print $2}')
 
-      key="$(create_etcd_key_from_env $env_key $ETCD_BASE_PATH)"
+      key=$(create_etcd_key_from_env "$env_key" "$ETCD_BASE_PATH")
 
-      if [[ (! -z "$key") && (! -z "$value") ]]; then
+      if [[ ! -z "$key" && ! -z "$value" ]]; then
         echo "publish $key = $value to $ETCD_ENDPOINT with a ttl of $ETCD_TTL"
-        echo "$(set_etcd_value $key $value $ETCD_TTL)"
+        echo $(set_etcd_value "$key" "$value" "$ETCD_TTL")
       fi
     fi
   done <<< "$envs"
