986ccac00b856df2b458899564d6565121b3940e
Gerard Meijer <gerard@meijer.gs>
2015-01-14 16:40:11 +0100

    "S()" changed to $(), because of unexpected results

--- docker/captain_iptables_discoverer/latest/files/scripts/run.sh
+++ docker/captain_iptables_discoverer/latest/files/scripts/run.sh
@@ -71,27 +71,27 @@ docker_nat_rules () {
     if [[ ! -z "$container_ip_with_key" ]]; then
       # remove all double quotes
       container_ip_with_key=${container_ip_with_key//\"/}
-      local container_ip_key="$(echo $container_ip_with_key | awk -F'=' '{print $1}')"
-      local container_ip="$(echo $container_ip_with_key | awk -F'=' '{print $2}')"
+      local container_ip_key=$(echo "$container_ip_with_key" | awk -F'=' '{print $1}')
+      local container_ip=$(echo "$container_ip_with_key" | awk -F'=' '{print $2}')
       local container_port_key=${container_ip_with_key/container_ip/container_port}
       local container_port_peer_key=${container_ip_with_key/container_ip/container_port_peer}
       local container_port_data_sync_key=${container_ip_with_key/container_ip/container_port_data_sync}
-      local container_port="$(get_etcd_value $container_port_key)"
-      local container_port_peer="$(get_etcd_value $container_port_peer_key)"
-      local container_port_data_sync="$(get_etcd_value $container_port_data_sync_key)"
+      local container_port=$(get_etcd_value "$container_port_key")
+      local container_port_peer=$(get_etcd_value "$container_port_peer_key")
+      local container_port_data_sync=$(get_etcd_value "$container_port_data_sync_key")
       
       if [[ ! -z "$container_port" ]]; then
-        local nat_rule="$(docker_nat_rule $container_ip $container_port)"
+        local nat_rule=$(docker_nat_rule "$container_ip" "$container_port")
         local nat_rules="$nat_rules$nat_rule"
       fi
       
       if [[ ! -z "$container_port_peer" ]]; then
-        local nat_rule="$(docker_nat_rule $container_ip $container_port_peer)"
+        local nat_rule=$(docker_nat_rule "$container_ip" "$container_port_peer")
         local nat_rules="$nat_rules$nat_rule"
       fi
       
       if [[ ! -z "$container_port_data_sync" ]]; then
-        local nat_rule="$(docker_nat_rule $container_ip $container_port_data_sync)"
+        local nat_rule=$(docker_nat_rule "$container_ip" "$container_port_data_sync")
         local nat_rules="$nat_rules$nat_rule"
       fi
       
@@ -112,13 +112,13 @@ docker_nat_rule () {
 }
 
 extra_rules () {
-  envs="$(env)"
+  envs=$(env)
   local extra_rules=""
 
   # every extra rule env starts with IPTABLES_RULE
   while read -r env; do
     if [[ $env == "IPTABLES_RULE_"* ]]; then
-      local extra_rule="$(echo $env | awk -F'=' '{print $2}')"
+      local extra_rule=$(echo "$env" | awk -F'=' '{print $2}')
       local extra_rule="$extra_rule"$'\n'
       local extra_rules="$extra_rules$extra_rule"
     fi
@@ -144,8 +144,8 @@ trusted_ip_rules () {
 }
 
 get_all_public_ips () {
-  local etcd_tree="$(get_etcd_tree $ETCD_BASE_PATH)"
-  local public_ips="$(echo $etcd_tree | $dir/jq '.nodes[] as $av_zones | $av_zones.nodes[] | select(.key | contains("/hosts")) | .nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/public_ip")) | .value')"
+  local etcd_tree=$(get_etcd_tree "$ETCD_BASE_PATH")
+  local public_ips=$(echo "$etcd_tree" | "$dir"/jq '.nodes[] as $av_zones | $av_zones.nodes[] | select(.key | contains("/hosts")) | .nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/public_ip")) | .value')
 
   echo "$public_ips"
 }
@@ -155,8 +155,8 @@ get_private_ips () {
 
   if [[ ! -z "$ETCD_CURRENT_AVZONE_PATH" ]]; then
     local etcd_tree_path="$ETCD_CURRENT_AVZONE_PATH""hosts/"
-    local etcd_tree="$(get_etcd_tree $etcd_tree_path)"
-    local private_ips="$(echo $etcd_tree | $dir/jq '.nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/private_ip")) | .value')"
+    local etcd_tree=$(get_etcd_tree "$etcd_tree_path")
+    local private_ips=$(echo "$etcd_tree" | "$dir"/jq '.nodes[] as $hosts | $hosts.nodes[] as $keys | $keys | select(.key | contains("/private_ip")) | .value')
   fi
 
   echo "$private_ips"
@@ -167,8 +167,8 @@ get_container_ips_with_keys () {
 
   if [[ ! -z "$ETCD_CURRENT_AVZONE_PATH" && ! -z "$CURRENT_HOST" ]]; then
     local etcd_tree_path="$ETCD_CURRENT_AVZONE_PATH""containers/"
-    local etcd_tree="$(get_etcd_tree $etcd_tree_path)"
-    local container_ips_with_keys="$(echo $etcd_tree | $dir/jq '.nodes[] as $containers | $containers.nodes[] as $hosts | $hosts | select(.key | contains("/'"$CURRENT_HOST"'")) as $host | $host.nodes[] as $keys | $keys | select(.key | contains("/container_ip")) | .key + "=" + .value')"
+    local etcd_tree=$(get_etcd_tree "$etcd_tree_path")
+    local container_ips_with_keys=$(echo "$etcd_tree" | "$dir"/jq '.nodes[] as $containers | $containers.nodes[] as $hosts | $hosts | select(.key | contains("/'"$CURRENT_HOST"'")) as $host | $host.nodes[] as $keys | $keys | select(.key | contains("/container_ip")) | .key + "=" + .value')
   fi
 
   echo "$container_ips_with_keys"
@@ -183,16 +183,16 @@ write_iptables_rules_file () {
 
   create_empty_file "$file_path" "$file_name"
 
-  local public_ips="$(get_all_public_ips)"
-  local private_ips="$(get_private_ips)"
-  local container_ips_with_keys="$(get_container_ips_with_keys)"
-  local public_ip_rules="$(trusted_ip_rules $public_ips)"
-  local private_ip_rules="$(trusted_ip_rules $private_ips)"
-  local container_nat_rules="$(docker_nat_rules container_ips_with_keys)"
-  local extra_rules="$(extra_rules)"
+  local public_ips=$(get_all_public_ips)
+  local private_ips=$(get_private_ips)
+  local container_ips_with_keys=$(get_container_ips_with_keys)
+  local public_ip_rules=$(trusted_ip_rules "$public_ips")
+  local private_ip_rules=$(trusted_ip_rules "$private_ips")
+  local container_nat_rules=$(docker_nat_rules "$container_ips_with_keys")
+  local extra_rules=$(extra_rules)
 
   # put all together
-  local complete_file_path="$(get_file_path_including_file_name $file_path $file_name)"
+  local complete_file_path=$(get_file_path_including_file_name "$file_path $file_name")
   echo "$iptables_filter_rules_start"$'\n' >> "$complete_file_path"
   if [[ ! -z "$public_ip_rules" ]]; then
     echo "# public ip lines" >> "$complete_file_path"
@@ -224,14 +224,14 @@ watch_iptables_rules_file () {
   local file_name="$2"
 
   local end_loop=false
-  local current_file="$(get_file_path_including_file_name $file_path $file_name)"
-  local current_rules="$(cat $current_file | sort)"
+  local current_file=$(get_file_path_including_file_name "$file_path" "$file_name")
+  local current_rules=$(cat "$current_file" | sort)
 
   while [[ "$end_loop" != true ]]; do
     file_name_watch="$file_name""_watch"
     write_iptables_rules_file "$file_path" "$file_name_watch"
-    local new_file="$(get_file_path_including_file_name $file_path $file_name_watch)"
-    local new_rules="$(cat $new_file | sort)"
+    local new_file=$(get_file_path_including_file_name "$file_path" "$file_name_watch")
+    local new_rules=$(cat "$new_file" | sort)
     if [[ "$current_rules" != "$new_rules" && ("$new_rules" == *"private ip lines"* || "$new_rules" == *"public ip lines"*) ]]; then
       local end_loop=true
     fi
