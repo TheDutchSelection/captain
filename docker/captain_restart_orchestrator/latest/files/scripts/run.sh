76eecbc19f7c198bbf4fc1bca07ee51ead8b9d53
Gerard Meijer <gerard@meijer.gs>
2015-01-14 11:06:25 +0100

    refactor

--- docker/captain_restart_orchestrator/latest/files/scripts/run.sh
+++ docker/captain_restart_orchestrator/latest/files/scripts/run.sh
@@ -8,65 +8,76 @@ dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/captain_functions"
 
-# $1: etcd need restart key
-set_restart_from_needs_restart_etcd_key () {
-  local need_restart_key="$1"
-  local restart_key=${need_restart_key/"$short_need_restart_key"/"$short_restart_key"}
-  local current_restart_value=$(get_etcd_value "$restart_key")
-  if [[ "$current_restart_value" != "$restart_value" ]]; then
-    set_etcd_value "$restart_key" "$restart_value"
-  fi
+# $1: etcd app path
+get_max_restarts_for_app () {
+  local etcd_app_path="$1"
+  local max_restarts=1
+
+  for app_exception in $APP_EXCEPTIONS
+  do
+    local partial_etcd_key="$(echo $app_exception | awk -F'##' '{print $1}')"
+    local max_restart_value="$(echo $app_exception | awk -F'##' '{print $2}')"
+    if [[ "/$partial_etcd_key" == *"$etcd_app_path"* ]]; then
+      max_restarts="$max_restart_value"
+      break
+    fi
+  done
+
+  echo "$max_restarts"
 }
 
-short_need_restart_key="$(echo $ETCD_NEED_RESTART_KEY_VALUE | awk -F'##' '{print $1}')"
-need_restart_value="$(echo $ETCD_NEED_RESTART_KEY_VALUE | awk -F'##' '{print $2}')"
-short_update_key="$(echo $ETCD_UPDATE_KEY_VALUE | awk -F'##' '{print $1}')"
-update_value="$(echo $ETCD_UPDATE_KEY_VALUE | awk -F'##' '{print $2}')"
-short_restart_key="$(echo $ETCD_RESTART_KEY_VALUE | awk -F'##' '{print $1}')"
-restart_value="$(echo $ETCD_RESTART_KEY_VALUE | awk -F'##' '{print $2}')"
+# $1: need restart keys
+# $2: partial need restart key
+# $4: partial update key
+# $5: update value
+# $6: partial restart key
+# $7: restart value
+# description: takes need restart keys and sets the corresponding restart keys to the restart value, after checking
+#              if the image is not updating or too many other containers are currently restarting
+set_restart_values_from_need_restart_keys () {
+  local need_restart_keys="$1"
+  local partial_need_restart_key="$2"
+  local partial_update_key="$3"
+  local update_value="$4"
+  local partial_restart_key="$5"
+  local restart_value="$6"
 
-echo "get $short_need_restart_key where value is $need_restart_value every $REFRESH_TIME seconds..."
-while true; do
-  need_restart_keys="$(get_etcd_keys_from_partial_key_with_value $ETCD_BASE_PATH $short_need_restart_key $need_restart_value)"
+  local result=""
 
-  # walk through all containers that need a restart
   while read -r need_restart_key; do
     if [[ ! -z "$need_restart_key" ]]; then
-      echo "$need_restart_key is $need_restart_value, container needs restart"
-      # get the relevant apps path
-      relevant_etcd_app_path="$(get_etcd_app_path_from_app_key $need_restart_key)"
+      local relevant_etcd_app_path="$(get_etcd_app_path_from_app_key $need_restart_key)"
+      local current_restarts="$(count_etcd_keys_with_value_for_app $relevant_etcd_app_path $restart_key $restart_value)"
+      local max_restarts="$(get_max_restarts_for_app $relevant_etcd_app_path)"
+      local current_update_value="$(get_etcd_value_from_other_key $need_restart_key $partial_need_restart_key $partial_update_key)"
 
-      echo "checking how many containers are restarting for $relevant_etcd_app_path..."
-      # count the current amount of restarts processing for the app path
-      current_restarts="$(count_etcd_keys_with_value_for_app $relevant_etcd_app_path $restart_key $restart_value)"
-      echo "$current_restarts"
-
-      # set the max restarts for this app
-      max_restarts=1
-      for app_exception in $APP_EXCEPTIONS
-      do
-        short_etcd_key="$(echo $app_exception | awk -F'##' '{print $1}')"
-        max_restart_value="$(echo $app_exception | awk -F'##' '{print $2}')"
-        if [[ "/$short_etcd_key" == *"$relevant_etcd_app_path"* ]]; then
-          max_restarts="$max_restart_value"
-          break
-        fi
-      done
-
-      echo "max restarts for $relevant_etcd_app_path is $max_restarts"
       if [[ "$max_restarts" > "$current_restarts" ]]; then
-        update_key=${need_restart_key/"$short_need_restart_key"/"$short_update_key"}
-        current_update_value=$(get_etcd_value "$update_key")
         if [[ "$current_update_value" = "$update_value" ]]; then
-          echo "currently updating, not restarting"
+          local result="$result"$'\n'"$need_restart_key - currently updating"
         else
-          echo "setting restart value for container with $need_restart_key"
-          set_restart_from_needs_restart_etcd_key "$need_restart_key"
+          local result="$result"$'\n'"$need_restart_key - set $partial_restart_key to $restart_value"
+          set_etcd_value_from_other_key "$need_restart_key" "$partial_need_restart_key" "$partial_restart_key" "$restart_value"
         fi
       else
-        echo "max restarts reached, not restarting"
+        local result="$result"$'\n'"$need_restart_key - max restarts reached"
       fi
     fi
   done <<< "$need_restart_keys"
+
+  echo "$result"
+}
+
+partial_need_restart_key="$(echo $ETCD_NEED_RESTART_KEY_VALUE | awk -F'##' '{print $1}')"
+need_restart_value="$(echo $ETCD_NEED_RESTART_KEY_VALUE | awk -F'##' '{print $2}')"
+partial_update_key="$(echo $ETCD_UPDATE_KEY_VALUE | awk -F'##' '{print $1}')"
+update_value="$(echo $ETCD_UPDATE_KEY_VALUE | awk -F'##' '{print $2}')"
+partial_restart_key="$(echo $ETCD_RESTART_KEY_VALUE | awk -F'##' '{print $1}')"
+restart_value="$(echo $ETCD_RESTART_KEY_VALUE | awk -F'##' '{print $2}')"
+
+echo "get $partial_need_restart_key where value is $need_restart_value every $REFRESH_TIME seconds..."
+while true; do
+  need_restart_keys="$(get_etcd_keys_from_partial_key_with_value $ETCD_BASE_PATH $partial_need_restart_key $need_restart_value)"
+
+  echo "$(set_restart_values_from_need_restart_keys $need_restart_keys $partial_need_restart_key $partial_update_key $update_value $partial_restart_key $restart_value)"
   sleep "$REFRESH_TIME"
 done
