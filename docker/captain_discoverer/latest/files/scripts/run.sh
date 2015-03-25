66d119afa022a44f389472c5b8cf2be5e9a28e7f
Gerard Meijer <gerard@meijer.gs>
2015-03-25 16:48:01 +0100

    added possibility for all host public ip envs

--- docker/captain_discoverer/latest/files/scripts/run.sh
+++ docker/captain_discoverer/latest/files/scripts/run.sh
@@ -8,14 +8,21 @@ dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/captain_functions"
 
-get_all_public_ips () {
+get_all_app_public_ips () {
   local etcd_tree=$(get_etcd_tree "$ETCD_BASE_PATH")
   local public_ips=$(echo "$etcd_tree" | jq '.nodes[] as $av_zones | $av_zones.nodes[] | select(.key | contains("/containers")) | .nodes[] as $apps | $apps.nodes[] as $app_ids | $app_ids.nodes[] | select(.key | contains("/host_public_ip")) | .key + "=" + .value')
 
   echo "$public_ips"
 }
 
-get_private_ips () {
+get_all_host_public_ips () {
+  local etcd_tree=$(get_etcd_tree "$ETCD_BASE_PATH")
+  local public_ips=$(echo "$etcd_tree" | jq '.nodes[] as $av_zones | $av_zones.nodes[] | select(.key | contains("/hosts")) | .nodes[] as $hosts | $hosts.nodes[] | select(.key | contains("/public_ip")) | .key + "=" + .value')
+
+  echo "$public_ips"
+}
+
+get_app_private_ips () {
   local private_ips=""
 
   if [[ ! -z "$ETCD_CURRENT_AVZONE_PATH" ]]; then
@@ -27,7 +34,7 @@ get_private_ips () {
   echo "$private_ips"
 }
 
-get_ports () {
+get_app_ports () {
   local ports=""
 
   local etcd_tree=$(get_etcd_tree "$ETCD_BASE_PATH")
@@ -36,12 +43,11 @@ get_ports () {
 }
 
 # $1: etcd key values
-create_envs () {
+create_app_envs () {
   local etcd_key_values="$1"
   local envs=""
 
   while read -r etcd_key_value; do
-    include_key=false
     for app_key in $APP_KEYS
     do
       if [[ "$etcd_key_value" == *"/$app_key"* && ( -z "$IGNORED_APP_KEY" || "$etcd_key_value" != *"$IGNORED_APP_KEY"*) ]]; then
@@ -58,6 +64,23 @@ create_envs () {
   echo "$envs"
 }
 
+# $1: etcd key values
+create_host_envs () {
+  local etcd_key_values="$1"
+  local envs=""
+
+  while read -r etcd_key_value; do
+    env=$(create_env_from_etcd_key_value "$etcd_key_value" "hosts/")
+    if [[ -z "$envs" ]]; then
+      envs="$env"
+    else
+      envs="$envs"$'\n'"$env"
+    fi
+  done <<< "$etcd_key_values"
+
+  echo "$envs"
+}
+
 # $1: file path
 # $1: file name
 write_container_environment_file () {
@@ -68,23 +91,30 @@ write_container_environment_file () {
   create_empty_file "$file_path" "$file_name"
 
   if [[ ! -z "$APP_KEYS" ]]; then
-    local public_ip_key_values=$(get_all_public_ips)
-    local private_ip_key_values=$(get_private_ips)
-    local port_key_values=$(get_ports)
-    local public_ip_envs=$(create_envs "$public_ip_key_values")
-    local private_ip_envs=$(create_envs "$private_ip_key_values")
-    local port_envs=$(create_envs "$port_key_values")
+    local app_public_ip_key_values=$(get_all_app_public_ips)
+    local app_private_ip_key_values=$(get_app_private_ips)
+    local app_port_key_values=$(get_app_ports)
+    local app_public_ip_envs=$(create_app_envs "$app_public_ip_key_values")
+    local app_private_ip_envs=$(create_app_envs "$app_private_ip_key_values")
+    local app_port_envs=$(create_app_envs "$app_port_key_values")
+    if [[ "$ALL_HOST_PUBLIC_IPS" == "1" ]]; then
+      local host_public_ip_key_values=$(get_all_host_public_ips)
+      local host_public_ip_envs=$(create_app_envs "$host_public_ip_key_values")
+    fi
 
     # put all together
     local complete_file_path=$(get_file_path_including_file_name "$file_path" "$file_name")
-    if [[ ! -z "$public_ip_envs" ]]; then
-      echo "$public_ip_envs" >> "$complete_file_path"
+    if [[ ! -z "$app_public_ip_envs" ]]; then
+      echo "$app_public_ip_envs" >> "$complete_file_path"
     fi
-    if [[ ! -z "$private_ip_envs" ]]; then
-      echo "$private_ip_envs" >> "$complete_file_path"
+    if [[ ! -z "$host_public_ip_envs" ]]; then
+      echo "$host_public_ip_envs" >> "$complete_file_path"
     fi
-    if [[ ! -z "$port_envs" ]]; then
-      echo "$port_envs" >> "$complete_file_path"
+    if [[ ! -z "$app_private_ip_envs" ]]; then
+      echo "$app_private_ip_envs" >> "$complete_file_path"
+    fi
+    if [[ ! -z "$app_port_envs" ]]; then
+      echo "$app_port_envs" >> "$complete_file_path"
     fi
   fi
 }
